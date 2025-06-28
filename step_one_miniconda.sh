1a61c21476f409db563f8f500f8b5d518b128241
ianplusplus <57041845+ianplusplus@users.noreply.github.com>
2025-06-28 13:33:24 -0500

    Update step_one_miniconda.sh
    Updated line endings to Unix and adjusted some errors in the script.

--- step_one_miniconda.sh
+++ step_one_miniconda.sh
@@ -12,28 +12,3 @@ chmod u+x Miniconda3-latest-Linux-x86_64.sh
 
 bash Miniconda3-latest-Linux-x86_64.sh
 source ~/.bashrc
-
-#install mamba
-
-conda install mamba -n base -c conda-forge
-
-#create an environment for fastai
-
-mamba create -n fastai python=3.10 -c conda-forge
-mamba activate fastai
-
-#look into what this line does I am not actually quite sure, but it makes fastai activate in conda
-
-eval "$(mamba shell hook --shell bash)"
-
-mamba activate fastai
-
-mamba shell init --shell bash --root-prefix=~/.local/share/mamba
-
-exec bash
-
-mamba activate fastai
-
-pip install fiftyone
-
-echo "To run Jupyter on LAN run the line jupyter lab --ip=0.0.0.0 --port=8888 --no-browser but replace the ip with yours"
